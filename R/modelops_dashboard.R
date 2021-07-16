8ac3551454f0df14e35f4c242fd2f4e119718036
Julia Silge <julia.silge@gmail.com>
2021-07-16 15:14:20 -0600

    Initial draft of modelops_dashboard

---
+++ R/modelops_dashboard.R
@@ -0,0 +1,17 @@
+#' R Markdown format for model monitoring dashboards
+#'
+#' @param ... Arguments passed to [flexdashboard::flex_dashboard()]
+#' @param display_pins Should the dashboard display a link to the pin(s)?
+#' Defaults to `TRUE`.
+#' @param display_api Should the dashboard display the OpenAPI documentation
+#' for the deployed model? Defaults to `TRUE`.
+#'
+#' @details The `modelops_dashboard()` function is a specialized type of
+#' \pkg{flexdashboard}. See the flexdashboard website for additional
+#' documentation:
+#'  \href{http://rmarkdown.rstudio.com/flexdashboard/}{http://rmarkdown.rstudio.com/flexdashboard/}
+#'
+#' @export
+modelops_dashboard <- function(..., display_pins = TRUE, display_api = TRUE) {
+    flexdashboard::flex_dashboard(...)
+}
