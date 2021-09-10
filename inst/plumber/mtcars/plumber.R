a82dd90d4603670bbfd852e962f87048e1b936da
Julia Silge <julia.silge@gmail.com>
2021-09-10 17:09:09 -0600

    Work on plumber file prototype

--- inst/plumber/mtcars/plumber.R
+++ inst/plumber/mtcars/plumber.R
@@ -1,9 +1,12 @@
+## auto generate this file
+
 library(modelops)
 library(pins)
 library(plumber)
 
 model_board <- board_rsconnect(server = "https://colorado.rstudio.com/rsc")
 m <- model_board %>% modelops_pin_read("julia.silge/cars_linear")
+stopifnot(m$metadata$version == "46968")
 
 #* @plumber
 function(pr) {
