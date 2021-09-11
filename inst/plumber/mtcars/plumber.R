0c024069692f86677b598ba699d983e9b5dfc92d
Julia Silge <julia.silge@gmail.com>
2021-09-10 20:20:40 -0600

    Update plumber file for options

--- inst/plumber/mtcars/plumber.R
+++ inst/plumber/mtcars/plumber.R
@@ -4,9 +4,12 @@ library(modelops)
 library(pins)
 library(plumber)
 
-model_board <- board_rsconnect(server = "https://colorado.rstudio.com/rsc")
-m <- model_board %>% modelops_pin_read("julia.silge/cars_linear")
+b <- board_rsconnect(server = "https://colorado.rstudio.com/rsc")
+m <- modelops_pin_read(b, "julia.silge/cars_linear")
 stopifnot(m$metadata$version == "46968")
+
+## or is it better to deploy with:
+## m <- modelops_pin_read(b, "julia.silge/cars_linear", version = "46968")
 
 #* @plumber
 function(pr) {
