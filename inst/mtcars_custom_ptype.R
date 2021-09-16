050a87d29f8317ee92fb6b60a2639125a8005b94
Julia Silge <julia.silge@gmail.com>
2021-09-16 16:00:45 -0600

    New example for working on custom ptype Swagger docs

---
+++ inst/mtcars_custom_ptype.R
@@ -0,0 +1,12 @@
+library(modelops)
+library(pins)
+library(plumber)
+
+model_board <- board_temp()
+cars_lm <- lm(mpg ~ cyl + disp, data = mtcars)
+m <- modelops(cars_lm, "cars_linear", model_board, ptype = mtcars[3:10, 2:3])
+modelops_pin_write(m)
+
+pr() %>%
+    modelops_pr_predict(m, debug = TRUE) %>%
+    pr_run(port = 8088)
