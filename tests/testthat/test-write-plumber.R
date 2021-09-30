201d0ea88f7b6ba28292230825473acda94f5265
Julia Silge <julia.silge@gmail.com>
2021-09-30 14:01:20 -0600

    Add test for no extra pkgs

--- tests/testthat/test-write-plumber.R
+++ tests/testthat/test-write-plumber.R
@@ -1,13 +1,24 @@
 library(pins)
 library(plumber)
 
-test_that("create plumber.R", {
+test_that("create plumber.R with packages", {
     skip_on_cran()
     b <- board_folder(path = "/tmp/test")
     tmp <- tempfile()
     cars_lm <- lm(mpg ~ cyl + disp, data = mtcars)
     m <- modelops(cars_lm, "cars1", b)
     m$metadata$required_pkgs <- c("beepr", "janeaustenr")
+    modelops_pin_write(m)
+    modelops_write_plumber(b, "cars1", file = tmp)
+    expect_snapshot(readr::read_lines(tmp))
+})
+
+test_that("create plumber.R with no packages", {
+    skip_on_cran()
+    b <- board_folder(path = "/tmp/test")
+    tmp <- tempfile()
+    cars_lm <- lm(mpg ~ cyl + disp, data = mtcars)
+    m <- modelops(cars_lm, "cars1", b)
     modelops_pin_write(m)
     modelops_write_plumber(b, "cars1", file = tmp)
     expect_snapshot(readr::read_lines(tmp))
