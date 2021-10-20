4f03c26cc6f50af19eac4d488c35ded6d99476c7
Julia Silge <julia.silge@gmail.com>
2021-10-20 14:15:49 -0600

    Use vec_ptype(). Closes #26.

--- R/create-ptype.R
+++ R/create-ptype.R
@@ -61,7 +61,7 @@ vetiver_slice_zero.default <- function(model, ...) {
 #' @export
 vetiver_slice_zero.lm <- function(model, ...) {
     pred_names <- attr(model$terms, "term.labels")
-    ptype <- vctrs::vec_slice(model$model[pred_names], 0)
+    ptype <- vctrs::vec_ptype(model$model[pred_names])
     tibble::as_tibble(ptype)
 }
 
