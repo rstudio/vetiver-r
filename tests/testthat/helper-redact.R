e2d7901324ba7fe08be31e1101dac72f12c98f01
Julia Silge <julia.silge@gmail.com>
2022-03-16 09:03:39 -0600

    Used `fixed = TRUE` for all testing transforms

--- tests/testthat/helper-redact.R
+++ tests/testthat/helper-redact.R
@@ -4,5 +4,5 @@ rel_dir <- fs::path_rel(tmp_dir)
 redact_vetiver <- function(snapshot) {
     snapshot <- gsub(rel_dir, "<redacted>", snapshot, fixed = TRUE)
     snapshot <- gsub(tmp_dir, "<redacted>", snapshot, fixed = TRUE)
-    snapshot <- gsub(getRversion(), "<r_version>", snapshot)
+    snapshot <- gsub(getRversion(), "<r_version>", snapshot, fixed = TRUE)
 }
