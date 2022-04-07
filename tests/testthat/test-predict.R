60143df282a45ae3d14235c3c248df25652d41e6
Julia Silge <julia.silge@gmail.com>
2022-04-06 21:06:31 -0600

    Bump up sleep :sleeping: MORE

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -3,7 +3,7 @@ skip_on_cran()
 test_that("router has health check endpoint", {
     pr <- pr() %>% vetiver_api(v)
     rs <- local_plumber_session(pr, port)
-    Sys.sleep(0.5)
+    Sys.sleep(1)
     r <- httr::GET(root_path, port = port, path = "ping")
     expect_equal(r$status_code, 200)
 })
@@ -11,7 +11,7 @@ test_that("router has health check endpoint", {
 test_that("can predict on basic vetiver router", {
     pr <- pr() %>% vetiver_api(v)
     rs <- local_plumber_session(pr, port)
-    Sys.sleep(0.5)
+    Sys.sleep(1)
     endpoint <- vetiver_endpoint(paste0(root_path, ":", port, "/predict"))
     preds <- predict(endpoint, mtcars[10:17, 2:3])
     expect_s3_class(preds, "tbl_df")
