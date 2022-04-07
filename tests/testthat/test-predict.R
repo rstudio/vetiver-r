c60e1c5ac981e791b1bc865d2aa45b2869bb4c58
Julia Silge <julia.silge@gmail.com>
2022-04-06 21:25:11 -0600

    Add expectation for endpoint

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -17,6 +17,7 @@ test_that("router has health check endpoint", {
 test_that("can predict on basic vetiver router", {
     endpoint <- vetiver_endpoint(paste0(root_path, ":", port, "/predict"))
     preds <- predict(endpoint, mtcars[10:17, 2:3])
+    expect_s3_class(endpoint, "vetiver_endpoint")
     expect_s3_class(preds, "tbl_df")
     expect_equal(nrow(preds), 8)
 })
