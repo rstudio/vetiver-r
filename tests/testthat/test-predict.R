228694698c722393942b67e8aa0d57fa65ee695d
Julia Silge <julia.silge@gmail.com>
2022-04-06 20:44:34 -0600

    Longer sleep for CI

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -1,7 +1,9 @@
+skip_on_cran()
+
 test_that("router has health check endpoint", {
     pr <- pr() %>% vetiver_api(v)
     rs <- local_plumber_session(pr, port)
-    Sys.sleep(0.2)
+    Sys.sleep(0.5)
     r <- httr::GET(root_path, port = port, path = "ping")
     expect_equal(r$status_code, 200)
 })
@@ -9,7 +11,7 @@ test_that("router has health check endpoint", {
 test_that("can predict on basic vetiver router", {
     pr <- pr() %>% vetiver_api(v)
     rs <- local_plumber_session(pr, port)
-    Sys.sleep(0.2)
+    Sys.sleep(0.5)
     endpoint <- vetiver_endpoint(paste0(root_path, ":", port, "/predict"))
     preds <- predict(endpoint, mtcars[10:17, 2:3])
     expect_s3_class(preds, "tbl_df")
