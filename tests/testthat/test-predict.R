c0d60279f60078be950a4d2961d220e3aaf32577
Julia Silge <julia.silge@gmail.com>
2022-04-06 19:14:34 -0600

    Move closing outside function

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -13,21 +13,19 @@ local_plumber_session <- function(pr, port, docs = FALSE) {
         },
         args = list(pr = pr, port = port, docs = docs)
     )
-    withr::defer(rs$close())
     rs
 }
+rs <- local_plumber_session(pr, port)
+withr::defer(rs$close())
 
 test_that("router has health check endpoint", {
-    rs <- local_plumber_session(pr, port)
     r <- httr::GET(root_path, port = port, path = "ping")
     expect_equal(r$status_code, 200)
 })
 
 test_that("can predict on basic vetiver router", {
-    rs <- local_plumber_session(pr, port)
     endpoint <- vetiver_endpoint(paste0(root_path, ":", port, "/predict"))
     preds <- predict(endpoint, mtcars[10:17, -1])
     expect_s3_class(preds, "tbl_df")
     expect_equal(nrow(preds), 8)
 })
-
