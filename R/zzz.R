2172be3472798c2ea63cbb64e5f3a59547ff0fd7
Julia Silge <julia.silge@gmail.com>
2022-11-04 16:40:50 -0600

    Try without the double `.onLoad` that may be happening here

--- R/zzz.R
+++ R/zzz.R
@@ -9,13 +9,6 @@ load_renv_env <- function() {
 
 .onLoad <- function(libname, pkgname) {
     load_renv_env()
-    renv$renv_platform_init()
-    renv$renv_envvars_init()
-    renv$renv_log_init()
-    renv$renv_methods_init()
-    renv$renv_filebacked_init()
-    renv$renv_libpaths_init()
-    renv$renv_patch_init()
 }
 
 # nocov end
