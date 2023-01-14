9b346f265d4ba93ca7bf1241ebbc82a97f1dc1d7
Julia Silge <julia.silge@gmail.com>
2023-01-13 17:27:41 -0700

    Example for keras

---
+++ inst/mtcars_keras.R
@@ -0,0 +1,29 @@
+library(vetiver)
+library(plumber)
+library(keras)
+
+scaled_cars <- as.matrix(mtcars) %>% scale()
+x_test  <- scaled_cars[26:32, 2:ncol(scaled_cars)]
+x_train <- scaled_cars[1:25, 2:ncol(scaled_cars)]
+y_train <- scaled_cars[1:25, 1]
+
+set.seed(1)
+
+keras_fit <-
+    keras_model_sequential()  %>%
+    layer_dense(units = 1, input_shape = ncol(x_train), activation = 'linear') %>%
+    compile(
+        loss = 'mean_squared_error',
+        optimizer = optimizer_adam(learning_rate = .01)
+    )
+
+keras_fit %>%
+    fit(
+        x = x_train, y = y_train,
+        epochs = 100, batch_size = 1,
+        verbose = 0
+    )
+
+v <- vetiver_model(keras_fit, "cars-keras", save_prototype = data.frame(x_train)[1,])
+pr() %>% vetiver_api(v, debug = TRUE) %>% pr_run()
+
