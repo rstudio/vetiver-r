81d8e58e609c53c010472dd3f84a4d1d6277391e
Julia Silge <julia.silge@gmail.com>
2023-02-07 15:36:34 -0700

    Fix testthat helper/setup

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -17,20 +17,3 @@ redact_vetiver <- function(snapshot) {
 redact_port <- function(snapshot) {
     snapshot <- gsub(port, "<port>", snapshot, fixed = TRUE)
 }
-
-
-clean_python_tmp_dir <- function(variables) {
-    if (rlang::is_installed("reticulate")) {
-        python_temp_dir <- dirname(reticulate::py_run_string(
-            "import tempfile; x=tempfile.NamedTemporaryFile().name",
-            local = TRUE
-        )$x)
-        detritus <- fs::dir_ls(
-            python_temp_dir,
-            regexp = "__autograph_generated_file|__pycache__"
-        )
-        fs::file_delete(detritus)
-    }
-}
-
-withr::defer(clean_python_tmp_dir(), teardown_env())
