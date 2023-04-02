56b30b3e6a12e019ab4b9d13a6616bb61fe90615
Julia Silge <julia.silge@gmail.com>
2023-04-02 15:43:37 -0600

    Reorder these tests to make more sense

--- tests/testthat/test-write-plumber.R
+++ tests/testthat/test-write-plumber.R
@@ -1,5 +1,19 @@
 skip_if_not_installed("plumber")
 
+test_that("create plumber.R with no packages", {
+    skip_on_cran()
+    b <- board_folder(path = tmp_dir)
+    tmp <- tempfile()
+    cars_lm <- lm(mpg ~ cyl + disp, data = mtcars)
+    v <- vetiver_model(cars_lm, "cars1")
+    vetiver_pin_write(b, v)
+    vetiver_write_plumber(b, "cars1", file = tmp)
+    expect_snapshot(
+        cat(readr::read_lines(tmp), sep = "\n"),
+        transform = redact_vetiver
+    )
+})
+
 test_that("create plumber.R with packages", {
     skip_on_cran()
     b <- board_folder(path = tmp_dir)
@@ -41,20 +55,6 @@ test_that("create plumber.R with args in dots", {
     vetiver_write_plumber(b, "cars1",
                           debug = TRUE, endpoint = "/predict2", type = "numeric",
                           file = tmp)
-    expect_snapshot(
-        cat(readr::read_lines(tmp), sep = "\n"),
-        transform = redact_vetiver
-    )
-})
-
-test_that("create plumber.R with no packages", {
-    skip_on_cran()
-    b <- board_folder(path = tmp_dir)
-    tmp <- tempfile()
-    cars_lm <- lm(mpg ~ cyl + disp, data = mtcars)
-    v <- vetiver_model(cars_lm, "cars1")
-    vetiver_pin_write(b, v)
-    vetiver_write_plumber(b, "cars1", file = tmp)
     expect_snapshot(
         cat(readr::read_lines(tmp), sep = "\n"),
         transform = redact_vetiver
