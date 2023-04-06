c11949a4084db614c9d3f41c898316309aea790a
Julia Silge <julia.silge@gmail.com>
2023-04-06 11:29:46 -0600

    Try attachNamespace

--- R/luz.R
+++ R/luz.R
@@ -34,8 +34,8 @@ vetiver_prepare_model.luz_module_fitted <- function(model) {
 #' @export
 handler_startup.luz_module_fitted <- function(vetiver_model) {
     attach_pkgs(vetiver_model$metadata$required_pkgs)
-    require("torch", quietly = TRUE)
-    require("luz", quietly = TRUE)
+    attachNamespace("torch")
+    attachNamespace("luz")
     print(sessionInfo())
 }
 
