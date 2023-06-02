bb44fa55a154664d9add1cc2990187b222e0f9df
Julia Silge <julia.silge@gmail.com>
2023-06-02 08:26:51 -0600

    In the future, need something like this

--- R/keras.R
+++ R/keras.R
@@ -61,5 +61,6 @@ handler_predict.keras.engine.training.Model <- function(vetiver_model, ...) {
 #' @rdname vetiver_python_requirements
 #' @export
 vetiver_python_requirements.keras.engine.training.Model <- function(model) {
+    ## TODO: something like pip freeze for keras and tensorflow to get versions
     system.file("requirements/keras-requirements.txt", package = "vetiver")
 }
