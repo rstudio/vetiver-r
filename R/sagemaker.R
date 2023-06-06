686ce2df2b5f52d1840211261d6918f9b2cfe021
Julia Silge <julia.silge@gmail.com>
2023-06-06 11:28:36 -0600

    Just clarify docs

--- R/sagemaker.R
+++ R/sagemaker.R
@@ -119,8 +119,11 @@ vetiver_deploy_sagemaker <- function(board,
 #' Defaults to `sagemaker-studio-${domain_id}:latest`.
 #' @param compute_type The [CodeBuild](https://aws.amazon.com/codebuild/)
 #' compute type as a character. Defaults to `BUILD_GENERAL1_SMALL`.
-#' @param build_role The IAM role name for CodeBuild to use as a character. Defaults
-#' to the SageMaker Studio execution role.
+#' @param role The ARM IAM role name (as a character) for:
+#' - CodeBuild to use for `vetiver_sm_build()`
+#' - the Amazon SageMaker model for `vetiver_sm_model()`
+#'
+#' Defaults to the SageMaker Studio execution role.
 #' @param bucket The S3 bucket to use for sending data to CodeBuild as a
 #' character. Defaults to the SageMaker SDK default bucket.
 #' @param vpc_id ID of the VPC that will host the CodeBuild project such as
@@ -189,7 +192,7 @@ vetiver_sm_build <- function(board,
                                  "BUILD_GENERAL1_LARGE",
                                  "BUILD_GENERAL1_2XLARGE"
                              ),
-                             build_role = NULL,
+                             role = NULL,
                              bucket = NULL,
                              vpc_id = NULL,
                              subnet_ids = list(),
@@ -214,7 +217,7 @@ vetiver_sm_build <- function(board,
     image_uri <- smdocker::sm_build(
         repository = repository,
         compute_type = compute_type,
-        role = build_role,
+        role = role,
         dir = path,
         bucket = bucket,
         vpc_id = vpc_id,
@@ -230,8 +233,6 @@ vetiver_sm_build <- function(board,
 #' @param image_uri The AWS ECR image URI for the Amazon SageMaker Model to be
 #' created (for example, as returned by [vetiver_sm_build()]).
 #' @param model_name The Amazon SageMaker model name to be deployed.
-#' @param model_role The ARN role for the Amazon SageMaker model. Defaults to the
-#' SageMaker execution role.
 #' @param vpc_config A list containing the VPC configuration for the Amazon
 #' SageMaker model [API VpcConfig](https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_VpcConfig.html)
 #' (optional).
@@ -245,7 +246,7 @@ vetiver_sm_build <- function(board,
 #' @export
 vetiver_sm_model <- function(image_uri,
                              model_name,
-                             model_role = NULL,
+                             role = NULL,
                              vpc_config = list(),
                              enable_network_isolation = FALSE,
                              tags = list()) {
@@ -257,15 +258,15 @@ vetiver_sm_model <- function(image_uri,
         model_name <- base_name_from_image(image_uri)
     }
 
-    if (is.null(model_role)) {
-        model_role <- smdocker::sagemaker_get_execution_role()
+    if (is.null(role)) {
+        role <- smdocker::sagemaker_get_execution_role()
     }
     tags <- sm_check_tags(tags)
     tags <- sm_format_tags(tags)
 
     request <- list(
         "ModelName" = model_name,
-        "ExecutionRoleArn" = model_role,
+        "ExecutionRoleArn" = role,
         "PrimaryContainer" = list("Image" = image_uri)
     )
     request$Tags <- .sm_append_project_tags(tags)
