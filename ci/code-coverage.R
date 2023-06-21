3cf1e59963d99dbcc08cfaff38df4afcce3c6e20
Kevin Ushey <kevinushey@gmail.com>
2023-06-20 22:51:00 -0800

    fixup

--- ci/code-coverage.R
+++ ci/code-coverage.R
@@ -1,5 +1,6 @@
 
-vetiver:::renv$the$log_level <- 1L
+renv <- vetiver:::renv
+renv$the$log_level <- 1L
 
 command <- paste("sleep 600 && kill -INT", Sys.getpid())
 system(command, wait = FALSE)
@@ -17,7 +18,7 @@ withCallingHandlers(
 
     error = function(cnd) {
         print(rlang::trace_back())
-        print(vetiver:::renv$renv_debuggify_dump(cnd))
+        print(renv$renv_debuggify_dump(cnd))
         stop(cnd)
     }
 
