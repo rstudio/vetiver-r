6bcb267da92de1c223ad5aad28b0a077ca57926d
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-10-07 15:11:57 -0700

    make tests more specific

--- tests/testthat/test-probably.R
+++ tests/testthat/test-probably.R
@@ -17,47 +17,47 @@ mtcars_wf <- workflow() %>%
     add_formula(mpg ~ .) %>%
     fit(data = mtcars)
 
-mtcars_int <- int_conformal_split(mtcars_wf, mtcars)
+mtcars_int_split <- int_conformal_split(mtcars_wf, mtcars)
 
-v <- vetiver_model(mtcars_int, "cars_int")
+v_split <- vetiver_model(mtcars_int_split, "cars_int_split")
 
 test_that("can print int_conformal_split model", {
-    expect_snapshot(v)
+    expect_snapshot(v_split)
 })
 
 test_that("can predict int_conformal_split model", {
-    preds <- predict(v, mtcars)
+    preds <- predict(v_split, mtcars)
     expect_s3_class(preds, "tbl_df")
     expect_equal(mean(preds$.pred), 20.1, tolerance = 0.1)
 })
 
 test_that("can pin a int_conformal_split model", {
     b <- board_temp()
-    vetiver_pin_write(b, v)
-    pinned <- pin_read(b, "cars_int")
+    vetiver_pin_write(b, v_split)
+    pinned <- pin_read(b, "cars_int_split")
     expect_equal(
         pinned,
         list(
-            model = bundle::bundle(butcher::butcher(mtcars_int)),
+            model = bundle::bundle(butcher::butcher(mtcars_int_split)),
             prototype = vctrs::vec_slice(tibble::as_tibble(mtcars[,2:11]), 0)
         ),
         ignore_formula_env = TRUE
     )
     expect_equal(
-        pin_meta(b, "cars_int")$user$required_pkgs,
+        pin_meta(b, "cars_int_split")$user$required_pkgs,
         c("parsnip", "ranger", "workflows", "probably")
     )
 })
 
 test_that("default endpoint for int_conformal_split", {
-    p <- pr() %>% vetiver_api(v)
+    p <- pr() %>% vetiver_api(v_split)
     p_routes <- p$routes[-1]
     expect_api_routes(p_routes)
 })
 
 test_that("default OpenAPI spec", {
-    v$metadata <- list(url = "potatoes")
-    p <- pr() %>% vetiver_api(v)
+    v_split$metadata <- list(url = "potatoes")
+    p <- pr() %>% vetiver_api(v_split)
     car_spec <- p$getApiSpec()
     expect_equal(car_spec$info$description,
                  "A Split Conformal inference with a ranger regression model")
@@ -73,9 +73,9 @@ test_that("default OpenAPI spec", {
 test_that("create plumber.R for int_conformal_split", {
     skip_on_cran()
     b <- board_folder(path = tmp_dir)
-    vetiver_pin_write(b, v)
+    vetiver_pin_write(b, v_split)
     tmp <- tempfile()
-    vetiver_write_plumber(b, "cars_int", file = tmp)
+    vetiver_write_plumber(b, "cars_int_split", file = tmp)
     expect_snapshot(
         cat(readr::read_lines(tmp), sep = "\n"),
         transform = redact_vetiver
