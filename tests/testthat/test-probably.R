eb64ed01896466d72c8a73948896d6bbc2477da4
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-10-07 15:20:40 -0700

    add support for probably::int_conformal_full()

--- tests/testthat/test-probably.R
+++ tests/testthat/test-probably.R
@@ -18,8 +18,10 @@ mtcars_wf <- workflow() %>%
     fit(data = mtcars)
 
 mtcars_int_split <- int_conformal_split(mtcars_wf, mtcars)
+mtcars_int_full <- int_conformal_full(mtcars_wf, mtcars)
 
 v_split <- vetiver_model(mtcars_int_split, "cars_int_split")
+v_full <- vetiver_model(mtcars_int_full, "cars_int_full")
 
 test_that("can print int_conformal_split model", {
     expect_snapshot(v_split)
@@ -76,6 +78,67 @@ test_that("create plumber.R for int_conformal_split", {
     vetiver_pin_write(b, v_split)
     tmp <- tempfile()
     vetiver_write_plumber(b, "cars_int_split", file = tmp)
+    expect_snapshot(
+        cat(readr::read_lines(tmp), sep = "\n"),
+        transform = redact_vetiver
+    )
+})
+
+test_that("can print int_conformal_full model", {
+    expect_snapshot(v_full)
+})
+
+test_that("can predict int_conformal_full model", {
+    preds <- predict(v_full, mtcars[1, ])
+    expect_s3_class(preds, "tbl_df")
+    expect_true(all(c(".pred_lower", ".pred_upper") %in% names(preds)))
+})
+
+test_that("can pin a int_conformal_full model", {
+    b <- board_temp()
+    vetiver_pin_write(b, v_full)
+    pinned <- pin_read(b, "cars_int_full")
+    expect_equal(
+        pinned,
+        list(
+            model = bundle::bundle(butcher::butcher(mtcars_int_full)),
+            prototype = vctrs::vec_slice(tibble::as_tibble(mtcars[,2:11]), 0)
+        ),
+        ignore_formula_env = TRUE
+    )
+    expect_equal(
+        pin_meta(b, "cars_int_full")$user$required_pkgs,
+        c("parsnip", "ranger", "workflows", "probably")
+    )
+})
+
+test_that("default endpoint for int_conformal_full", {
+    p <- pr() %>% vetiver_api(v_full)
+    p_routes <- p$routes[-1]
+    expect_api_routes(p_routes)
+})
+
+test_that("default OpenAPI spec", {
+    v_full$metadata <- list(url = "potatoes")
+    p <- pr() %>% vetiver_api(v_full)
+    car_spec <- p$getApiSpec()
+    expect_equal(car_spec$info$description,
+                 "A full Conformal inference with a ranger regression model")
+    post_spec <- car_spec$paths$`/predict`$post
+    expect_equal(names(post_spec), c("summary", "requestBody", "responses"))
+    expect_equal(as.character(post_spec$summary),
+                 "Return predictions from model using 10 features")
+    get_spec <- car_spec$paths$`/pin-url`$get
+    expect_equal(as.character(get_spec$summary),
+                 "Get URL of pinned vetiver model")
+})
+
+test_that("create plumber.R for int_conformal_full", {
+    skip_on_cran()
+    b <- board_folder(path = tmp_dir)
+    vetiver_pin_write(b, v_full)
+    tmp <- tempfile()
+    vetiver_write_plumber(b, "cars_int_full", file = tmp)
     expect_snapshot(
         cat(readr::read_lines(tmp), sep = "\n"),
         transform = redact_vetiver
